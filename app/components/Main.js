543d10c260ff1695bd36a29ed226ac85fca69f20
Ryan Van Ess <fallen.rve@gmail.com>
2016-11-13 20:55:34 -0600

    adds react routes, cleans up index

---
+++ app/components/Main.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+var Main = React.createClass({
+  render: function () {
+    return (
+      <div className='main-container'>
+        {this.props.children}
+      </div>
+    )
+  }
+});
+
+module.exports = Main;
